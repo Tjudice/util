003187f0865413f4beb2fde3bdb334fbba1a4991
tjudice <tjudice@umich.edu>
2023-08-08 02:01:47 -0500

    WindowFn

--- go/lambda/window.go
+++ go/lambda/window.go
@@ -12,15 +12,25 @@ package lambda
 func Window[T any](preceding, following int, items []T) [][]T {
 	windows := make([][]T, 0, len(items))
 	for i := range items {
-		precedingIdx := i - preceding
-		if precedingIdx < 0 {
-			precedingIdx = 0
-		}
-		followingIdx := i + following + 1
-		if followingIdx > len(items) {
-			followingIdx = len(items)
-		}
-		windows = append(windows, items[precedingIdx:followingIdx:followingIdx])
+		windows = append(windows, makeWindow(i, preceding, following, items))
 	}
 	return windows
+}
+
+func WindowFn[T any](preceding, following int, items []T, fn func([]T)) {
+	for i := range items {
+		fn(makeWindow(i, preceding, following, items))
+	}
+}
+
+func makeWindow[T any](idx, preceding, following int, items []T) []T {
+	precedingIdx := idx - preceding
+	if precedingIdx < 0 {
+		precedingIdx = 0
+	}
+	followingIdx := idx + following + 1
+	if followingIdx > len(items) {
+		followingIdx = len(items)
+	}
+	return items[precedingIdx:followingIdx:followingIdx]
 }
