b2e2f6a29b3c9a5d7c7197727b34d1035274b7ea
tjudice <tjudice@umich.edu>
2023-08-08 01:13:37 -0500

    modify priority fetcher to also take in interface

--- go/concurrency/prioritized_request_queue.go
+++ go/concurrency/prioritized_request_queue.go
@@ -4,10 +4,31 @@ import (
 	"context"
 )
 
+type PriorityClient[T, V any] interface {
+	// Less determines the ordering of fetching when the request queue is clogged,
+	// i.e. when the number of available workers is 0
+	Less(x, y T) bool
+	// Fetch fetches the result of a given request. Requets will be fed to Fetch() based on the ordering as defined by Less(T,T)
+	Fetch(context.Context, T) (V, error)
+}
+
+type PrioritizedRequestHandler[T, V any] struct {
+	less  func(T, T) bool
+	fetch func(context.Context, T) (V, error)
+}
+
+func (p *PrioritizedRequestHandler[T, V]) Less(x, y T) bool {
+	return p.less(x, y)
+}
+
+func (p *PrioritizedRequestHandler[T, V]) Fetch(ctx context.Context, params T) (V, error) {
+	return p.fetch(ctx, params)
+}
+
 type PrioritizedRequestQueue[T, V any] struct {
 	pq *PriorityQueue[request[T, V]]
 
-	getter func(context.Context, T) (V, error)
+	fetcher PriorityClient[T, V]
 
 	reqs chan struct{}
 
@@ -25,12 +46,24 @@ type response[V any] struct {
 	err error
 }
 
-func NewPrioritizedRequestQueue[T, V any](workers int, less func(i T, j T) bool, getter func(context.Context, T) (V, error)) *PrioritizedRequestQueue[T, V] {
+func NewPrioritizedRequestQueue[T, V any](workers int, less func(i T, j T) bool, fetch func(context.Context, T) (V, error)) *PrioritizedRequestQueue[T, V] {
+	f := &PrioritizedRequestHandler[T, V]{less: less, fetch: fetch}
 	o := &PrioritizedRequestQueue[T, V]{
-		pq:     NewPriorityQueue[request[T, V]](workers*10, makeLessFn[T, V](less)),
-		reqs:   make(chan struct{}, workers),
-		done:   make(chan struct{}),
-		getter: getter,
+		pq:      NewPriorityQueue[request[T, V]](workers*10, makeLessFn[T, V](f.Less)),
+		reqs:    make(chan struct{}, workers),
+		done:    make(chan struct{}),
+		fetcher: f,
+	}
+	o.run(workers)
+	return o
+}
+
+func NewPrioritizedRequestQueueInterface[T, V any](workers int, fetcher PriorityClient[T, V]) *PrioritizedRequestQueue[T, V] {
+	o := &PrioritizedRequestQueue[T, V]{
+		pq:      NewPriorityQueue[request[T, V]](workers*10, makeLessFn[T, V](fetcher.Less)),
+		reqs:    make(chan struct{}, workers),
+		done:    make(chan struct{}),
+		fetcher: fetcher,
 	}
 	o.run(workers)
 	return o
@@ -52,7 +85,7 @@ func (p *PrioritizedRequestQueue[T, V]) work() {
 			if !ok {
 				continue
 			}
-			res, err := p.getter(req.ctx, req.param)
+			res, err := p.fetcher.Fetch(req.ctx, req.param)
 			req.ch <- &response[V]{val: res, err: err}
 		}
 	}
