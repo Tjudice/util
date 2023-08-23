835cef603913a11e5df726748734e26ec0dfbb00
tjudice <tjudice@umich.edu>
2023-08-23 00:14:04 -0500

    transform slice, slice to any

--- go/lambda/slice.go
+++ go/lambda/slice.go
@@ -7,3 +7,17 @@ func MapToSlice[K comparable, V any](m map[K]V) []V {
 	}
 	return res
 }
+
+func TransformSlice[T, K any](vals []T, transformer func(T) K) []K {
+	n := make([]K, 0, len(vals))
+	for _, v := range vals {
+		n = append(n, transformer(v))
+	}
+	return n
+}
+
+func SliceToAny[T any](vals []T) []any {
+	return TransformSlice(vals, func(v T) any {
+		return v
+	})
+}
