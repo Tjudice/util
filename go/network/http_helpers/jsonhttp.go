35b86d9f4f81c672ef8f3744ce8a4b2adc81f7e2
tjudice <tjudice@umich.edu>
2023-08-22 23:44:44 -0500

    get json middleware

--- go/network/http_helpers/jsonhttp.go
+++ go/network/http_helpers/jsonhttp.go
@@ -13,6 +13,21 @@ func GetJSON[T any](ctx context.Context, cl *http.Client, url string, requestBod
 		return resp, err
 	}
 	req.Header.Add("content-type", "application/json")
+	return getJSON[T](ctx, cl, req)
+}
+
+func GetJSONFn[T any](ctx context.Context, cl *http.Client, url string, requestBody io.Reader, middleware func(r *http.Request)) (resp T, err error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, requestBody)
+	if err != nil {
+		return resp, err
+	}
+	req.Header.Add("content-type", "application/json")
+	// call middleware after all request modifications are made
+	middleware(req)
+	return getJSON[T](ctx, cl, req)
+}
+
+func getJSON[T any](ctx context.Context, cl *http.Client, req *http.Request) (resp T, err error) {
 	res, err := cl.Do(req)
 	if err != nil {
 		return resp, err
